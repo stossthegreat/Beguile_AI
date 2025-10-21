e4c1bcae972a90e3a0b3a218d3c2ca37d9493347
stossthegreat <daazawaaza@proton.me>
2025-10-21 04:41:34 +0100

    Update podhelper.rb

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,14 +1,16 @@
+# Flutter's iOS CocoaPods helper
 def flutter_root
   generated_xcode_build_settings_path = File.expand_path(File.join('..', 'Generated.xcconfig'), __FILE__)
   unless File.exist?(generated_xcode_build_settings_path)
-    raise "#{generated_xcode_build_settings_path} must exist. If you're running pod install manually, make sure flutter pub get is executed first."
+    raise "#{generated_xcode_build_settings_path} must exist. Run 'flutter pub get' first."
   end
 
   File.foreach(generated_xcode_build_settings_path) do |line|
     matches = line.match(/FLUTTER_ROOT\=(.*)/)
     return matches[1].strip if matches
   end
-  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}. Try running flutter pub get again."
+
+  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}."
 end
 
 require File.expand_path(File.join('packages', 'flutter_tools', 'bin', 'podhelper'), flutter_root)
